02a571ed840267dd09e7388f434770a529ac3fcb
mstop4 <mstop4@gmail.com>
2018-02-17 20:16:02 -0500

    added error listener. required for ws@>3.0.0

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ wss.on('connection', (ws) => {
     wss.broadcast(outMsg)
   })
 
+  ws.on('error', (e) => console.log(`An error has occured: ${e}`))
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log(`Client ${clientID} disconnected. ${wss.clients.size} clients online.`)
